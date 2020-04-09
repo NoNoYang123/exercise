519c3c163acc52183336be2e768c1dc07fd1ab83
NoNoYang <18930330261@163.com>
2020-04-09 14:54:20 +0800

    python 异常捕获

---
+++ demo05.py
@@ -0,0 +1,29 @@
+#异常捕获
+try:
+    print(2+"123")
+except:
+    print("出问题了")
+
+#异常类
+try:
+    print(2+jdfg)
+except Exception as e:
+    print("出问题了",e)
+
+#代码的方法
+#包--模块--类--方法--变量
+#导入自带包
+import time
+import random
+for i in range(10):
+    time.sleep(1) #停顿一秒后再进行下一个操作
+    print(i)
+a=random.randint(1,100)
+print(a)
+
+#第三方包
+'''
+cmd中输入 pip3 list 查看已有的包
+        pip3 install 安装包
+'''
+
